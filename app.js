b9786c60b2dee07951dd870a8fa8b1b9e5b92c9e
pratikb0501 <pratikbagmare@gmail.com>
2021-09-21 19:37:08 +0530

    created two textboxes and get their values

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("script")
+let translateButton = document.querySelector("#btn-translate");
+let inputArea = document.querySelector("#txtarea--input");
+let outputArea = document.querySelector("#txtarea--output");
+
+function handleClick() {
+    outputArea.innerHTML = (inputArea.value)
+}
+translateButton.addEventListener("click", handleClick)
